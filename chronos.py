92cb653ab6278cf76df2a1e9f482853404c44fb2
Katherine Wu <kwu16@jhu.edu>
2011-11-15 00:38:10 -0500

    more

--- chronos.py
+++ chronos.py
@@ -1,37 +1,29 @@
+"""
+Chronos energy
+"""
 ## beginning of import
 from __future__ import division
-import pygame, rabbyt, sys
-
-from math import cos, sin, radians
-import random
-import os.path
-
-import settings
-
-#rabbyt.data_directory = os.path.dirname(__file__)
-#rabbyt.set_default_attribs()
-
-## end of import
-
-#rabbyt.set_viewport((800,600))
+import pygame, rabbyt
 
 class Spark(rabbyt.Sprite):
+    """Chronos Energy"""
+    def __init__(self, screen, xy):
+        rabbyt.Sprite.__init__(self, '1energy.png')
+  
+        self.speed = -3
+        self.xy = xy
+        self.rot = 0
 
-   def __init__(self, screen, xy):
-      rabbyt.Sprite.__init__(self, '1energy.png')
+        self.time_last = pygame.time.get_ticks() 
+        self.screen = screen
+ 
+    def update(self):
+        """update method"""
+        self.y += self.speed
+        self.rot += 0.4
 
-      self.speed = -3
-      self.xy = xy
-      self.rot = 0
-
-      self.time_last = pygame.time.get_ticks() 
-      self.screen = screen
-
-   def update(self):
-       self.y += self.speed
-       self.rot += 0.4
-
-   def checkBounds(self):
-       if self.x >= 350 or self.y >= 450 or self.x <= -350 or self.y <= -450:
-           return True
-       return False
+    def checkBounds(self):
+        """checks bounds"""
+        if self.x >= 350 or self.y >= 450 or self.x <= -350 or self.y <= -450:
+            return True
+        return False
--- game_object.py
+++ game_object.py
@@ -1,17 +1,18 @@
+"""
+Game Object
+"""
+
 class GameObject():
+    """Game object"""
     def __init__(self):
         self.health = 0
         self.damage = 0
         self.point_value = 0
 
     def hit(self, damage=1):
+        """defines hit function"""
         self.health -= damage
 
     def die(self):
+        """Death"""
         return self.point_value
-
-	def isOffMap(self):
-		if self.y <= -350:
-			return True
-		else:
-			return False
