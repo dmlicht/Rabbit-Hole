3da253b6ad3765045d3ff07d8c41bd330ccb37a8
Owen Whitmore <owen171@comcast.net>
2011-11-30 16:07:41 -0500

    Added Final Cutscene

---
+++ states/cutfive.py
@@ -0,0 +1,65 @@
+"""
+The Fifth Cutscene
+"""
+from __future__ import division
+import pygame
+import rabbyt
+from pygame.locals import QUIT, KEYDOWN, K_RETURN, K_ESCAPE
+
+from settings import FontSprite
+import state
+import states.highscore
+
+#def CutScreen(game, state_stack):
+class CutFive(state.State):
+    """The Cutscene Class"""
+    def run(self, game, state_stack):
+        """Makes the Cutscene run"""
+        game.set_state_time()
+        scene = rabbyt.Sprite("1cutscene.png")
+        scene.scale = 0.95
+        scene.y = -60
+        scene.alpha = rabbyt.lerp(0.0, 0.8, startt=1, endt=8)
+        scene.x = rabbyt.lerp(-20, 60, startt=1, endt=6)
+
+        scene2 = rabbyt.Sprite("1cutscene2.png")
+        scene2.alpha = rabbyt.lerp(0.8, 0.0, startt=6, endt=13)
+        scene2.scale = 0.95
+        scene2.xy = (60, -60)
+
+        words = FontSprite(game.font, "Scientist: I'm Back!")
+        words.xy = (-200, -250)
+
+        game.done = False
+        while not game.done:
+            rabbyt.clear()
+            rabbyt.set_time(game.get_ticks()/1000.0)
+            ticks = game.get_ticks()/1000.0
+            if ticks < 6:
+                scene.render()
+            elif ticks >= 6 and ticks < 10:
+                words = FontSprite(game.font, "Scientist: And I brought flowers!")
+                words.xy = (-200, -250)
+                scene2.render()
+            elif ticks >= 10 and ticks <14:
+                words = FontSprite(game.font, "Scientist:Do you want to go back for more?")
+                words.xy = (-200, -250)
+                scene2.render()    
+            if ticks >= 14:
+                game.done = True
+                state_stack.append(states.highscore.High())
+            words.render()
+
+            for event in pygame.event.get():
+                if event.type ==  QUIT:
+                    game.done = True
+                    fdata = open("RabbitHighScores", 'w')
+                    for i in range(5):
+                        fdata.write(game.high_score_names[i] + " " \
+                                    + str(game.high_scores[i]) + "\n")
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE or event.key == K_RETURN:
+                        game.done = True
+                        state_stack.append(states.highscore.High())
+            pygame.display.flip()
+
